8beaefff407f23c032c29b59faeb3b8054d35630
Chiranjeev <chiranjeevbansal@gmail.com>
2025-08-14 17:30:47 +0530

    Add signup functionality 2

--- Frontend/whiteboard-frontend/src/App.js
+++ Frontend/whiteboard-frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import CanvasPage from "./pages/Canvas";
+import Signup from "./pages/Signup";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} /> 
         <Route path="/profile" element={<Profile />} />
         <Route path="/canvas/:canvasId" element={<CanvasPage />} />
       </Routes>
